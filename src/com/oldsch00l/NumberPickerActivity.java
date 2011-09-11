3ec7a8fae8d869dc2ab6827b3f60f26ebfc26156
Peinthor Rene <peinthor@gmail.com>
2011-09-11 21:22:07 +0200

    initial numberpicker widget

---
+++ src/com/oldsch00l/NumberPickerActivity.java
@@ -0,0 +1,13 @@
+package com.oldsch00l;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NumberPickerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
